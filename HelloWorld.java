25558a27fa82be8fc1fd21f7b17b203bbbf16592
Adam H <sinister3665@gmail.com>
2026-01-28 14:57:01 -0500

    :

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, Worlds!");
-=======
-        System.out.println("Hello, Worlds!");
->>>>>>> d71c71d1f5913c49afdf827d2bc149dbfae87fb7
+        System.out.println("Hello, Worlds 2!");
     }
 }
